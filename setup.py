c8db9c90c8e61606784bb438ffa498a1830d2e86
Amal <amalsaga@gmail.com>
2021-05-27 19:01:00 +0530

    tox_setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# To make it as a package
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Quality package package",
+    author="amal",
+    packages=find_packages(),
+    license="MIT"
+)
+
---
+++ tests/test_config.py
@@ -0,0 +1,4 @@
+def test_case():
+    a=3
+    b=3
+    assert a==b
